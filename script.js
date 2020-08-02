35417ca21f348142162619e3b1b8f83028362edd
Adam <irinaana.adam@tecknoworks.com>
2020-08-02 11:20:56 +0300

    version1.0.0

---
+++ script.js
@@ -0,0 +1,19 @@
+var arr = [1,2,3,4,5,6];
+var x = 6;
+var rez = divv(summ(arr), x);
+console.log(rez);
+
+
+function divv(q, p){
+    var z = q/p;
+    return z;
+}
+
+function summ(arr)
+{
+var sum = arr.reduce(function(a, b)
+    {
+      return a + b;
+    }, 0);
+    return sum;
+}
